e42e77e32f54ea21588216ffbf0356d701d32a8e
HOSLACK <hoslackochieng@gmail.com>
2017-04-04 10:18:18 +0300

    Refined prime function with data validation measures

--- prime.py
+++ prime.py
@@ -1,6 +1,9 @@
 def primenumber(n):
     result = []
-
+    if not isinstance(n, int):
+        return TypeError
+    elif n <= 0 or n >= 10000:
+        return ValueError
     for num in range(0, n+1):
         if all(num % i != 0 for i in range(2, num)):
             result.append(num)
