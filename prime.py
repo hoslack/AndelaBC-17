f4adb93a9d5b8355b2c82f86de35b75b70ded94b
HOSLACK <hoslackochieng@gmail.com>
2017-04-04 09:56:26 +0300

    A little correction in return value of prime function

--- prime.py
+++ prime.py
@@ -1,6 +1,8 @@
 def primenumber(n):
+    result = []
 
     for num in range(0, n+1):
         if all(num % i != 0 for i in range(2, num)):
-            print num
+            result.append(num)
+    return result
 
