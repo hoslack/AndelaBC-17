cac620a03dc1988bcdb07e649790e9d352f2e752
HOSLACK <hoslackochieng@gmail.com>
2017-04-04 09:54:24 +0300

    Unit tests for good result and correctness

--- primetests.py
+++ primetests.py
@@ -1,1 +1,16 @@
-primetests.py
+import unittest
+from prime import primenumber
+
+
+class PrimeTests(unittest.TestCase):
+    def test_result(self):
+        result = primenumber(10)
+        self.assertEqual(result, [0, 1, 2, 3, 5, 7])
+
+    def test_result_is_list(self):
+        result = primenumber(10)
+        self.assertTrue(result, list)
+
+
+if __name__ == '__main__':
+    unittest.main()
